b0c76c119c61b565962140eb7799c0df9a8a3cf8
Anthony <anthony.digangi@ge.com>
2017-02-13 22:55:57 -0500

    finishing shoa nd create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,13 +4,23 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    #render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect_to_articles_show(@article)
+    #@article.save
+    #redirect_to article_path(@article)
+    if @article.save
+      #do something
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
   end
 
   def show
+    @article = Article.find(params[:id])
+  end
+  
+  def destroy
     
   end
 
