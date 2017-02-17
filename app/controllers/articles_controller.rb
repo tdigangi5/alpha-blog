8d13c389fbf4d75666cad4c706df05c6eb26bb89
Anthony <anthony.digangi@ge.com>
2017-02-17 16:32:15 -0500

    Adding method for finding records

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+  
   def index
     @articles = Article.all
   end
@@ -21,15 +23,14 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+    
   end
   
   def edit
-    @article = Article.find(params[:id])
+    
   end
   
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
@@ -39,12 +40,18 @@ class ArticlesController < ApplicationController
   end
   
   def destroy
-    
+    @article.destroy
+    flash[:notice] = "Article was deleted"
+    redirect_to articles_path
   end
 
   private
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+  
+  def set_article
+     @article = Article.find(params[:id])
   end
 
 end
